7e5cbcea32ae8f89530ecb2b73e5f86f8762e4ac
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2024-02-23 18:40:49 +0200

    add files

---
+++ src/js/swiper.js
@@ -0,0 +1,16 @@
+new Swiper('.mySwiper',{
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    clickable: true,
+  },
+
+})
