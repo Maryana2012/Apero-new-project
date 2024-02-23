965e0b1576ad9e087bc684743c758c342aca8f25
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2024-02-23 18:33:16 +0000

    Deploying to gh-pages from @ Maryana2012/Apero-new-project@b4562fb4305c45f48b3745a944108ca7b3ea627d 🚀

---
+++ index.0052a4fc.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{autoplay:{delay:2500,disableOnInteraction:!1},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",dynamicBullets:!0,clickable:!0}});
+//# sourceMappingURL=index.0052a4fc.js.map
--- index.33696b32.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".mySwiper",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",dynamicBullets:!0,clickable:!0}});
-//# sourceMappingURL=index.33696b32.js.map
