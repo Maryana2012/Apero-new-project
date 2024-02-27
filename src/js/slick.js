b6b47a718ba853a76a02ff89097484d1f64ec5cd
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2024-02-27 18:28:11 +0200

    section partners animation

---
+++ src/js/slick.js
@@ -0,0 +1,21 @@
+// $(document).ready(function(){
+//     $('.slider').slick({
+//         infinite: true,
+//         slidesToShow: 3, // Кількість видимих слайдів одночасно
+//         slidesToScroll: 1, // Кількість слайдів, які прокручуються за один раз
+//         autoplay: true, // Автоматичне прокручування
+//         autoplaySpeed: 200 // Швидкість автоматичного прокручування в мілісекундах
+//     });
+//   });
+
+  // document.addEventListener('DOMContentLoaded', function() {
+  //   $('.slider').slick({
+  //       // infinite: true,
+  //       slidesToShow: 3, // Кількість видимих слайдів одночасно
+  //       slidesToScroll: 1, // Кількість слайдів, які прокручуються за один раз
+  //       autoplay: true, // Автоматичне прокручування
+  //       autoplaySpeed: 2500 // Швидкість автоматичного прокручування в мілісекундах
+  //   });
+  // });
+
+  
