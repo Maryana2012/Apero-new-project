7e6802a3ed68fe30d51d9a18103c1fef366e2eaa
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2024-03-19 12:52:58 +0200

    add Slick for images

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,21 +1,17 @@
-// $(document).ready(function(){
-//     $('.slider').slick({
-//         infinite: true,
-//         slidesToShow: 3, // Кількість видимих слайдів одночасно
-//         slidesToScroll: 1, // Кількість слайдів, які прокручуються за один раз
-//         autoplay: true, // Автоматичне прокручування
-//         autoplaySpeed: 200 // Швидкість автоматичного прокручування в мілісекундах
-//     });
-//   });
-
-  // document.addEventListener('DOMContentLoaded', function() {
-  //   $('.slider').slick({
-  //       // infinite: true,
-  //       slidesToShow: 3, // Кількість видимих слайдів одночасно
-  //       slidesToScroll: 1, // Кількість слайдів, які прокручуються за один раз
-  //       autoplay: true, // Автоматичне прокручування
-  //       autoplaySpeed: 2500 // Швидкість автоматичного прокручування в мілісекундах
-  //   });
-  // });
-
+$(document).ready(function(){
+  $('.partners__blocks-first').slick(
+    {
+      autoplay: true,
+      autoplaySpeed: 2000
+    }
+  );
+});
   
+$(document).ready(function(){
+  $('.mySwiper').slick(
+    {
+      autoplay: true,
+      autoplaySpeed: 2000
+    }
+  );
+});
