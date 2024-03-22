1cb734c47e69beda84894a8742620b55bf447f81
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2024-03-22 16:48:11 +0000

    Deploying to gh-pages from @ Maryana2012/Apero-new-project@9669eb453200691876cfc03813c951e382f8667b 🚀

--- index.0877bd88.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".partners__blocks-first").slick({autoplay:!0,autoplaySpeed:2e3})})),$(document).ready((function(){$(".mySwiper").slick({autoplay:!0,autoplaySpeed:2e3})}));
-//# sourceMappingURL=index.0877bd88.js.map
--- index.907c77d2.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".partners__blocks-first").slick({autoplay:!0,autoplaySpeed:2e3})})),$(document).ready((function(){$(".mySwiper").slick({autoplay:!0,autoplaySpeed:2e3})}));
-//# sourceMappingURL=index.907c77d2.js.map
