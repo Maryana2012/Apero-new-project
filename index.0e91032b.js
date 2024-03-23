36d62ec660b44c5fa5c12f7d575cff024b8f6046
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2024-03-23 18:53:16 +0000

    Deploying to gh-pages from @ Maryana2012/Apero-new-project@478e80650669994f44029189613af607d5975a6d 🚀

---
+++ index.0e91032b.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".mySwiper").slick({autoplay:!0,autoplaySpeed:2e3})}));
+//# sourceMappingURL=index.0e91032b.js.map
---
+++ index.c103352c.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".mySwiper").slick({autoplay:!0,autoplaySpeed:2e3})}));
+//# sourceMappingURL=index.c103352c.js.map
