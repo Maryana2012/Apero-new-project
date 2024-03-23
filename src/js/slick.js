478e80650669994f44029189613af607d5975a6d
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2024-03-23 20:52:51 +0200

    animation for partners block

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
-  $('.partners__blocks-first').slick(
-    {
-      autoplay: true,
-      autoplaySpeed: 2000
-    }
-  );
-});
+// $(document).ready(function(){
+//   $('.partners__blocks-first').slick(
+//     {
+//       autoplay: true,
+//       autoplaySpeed: 2000
+//     }
+//   );
+// });
   
 $(document).ready(function(){
   $('.mySwiper').slick(
