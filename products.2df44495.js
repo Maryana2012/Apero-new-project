2081a16f3afd371d7745b2c2debb044278db8079
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2024-03-25 19:51:43 +0000

    Deploying to gh-pages from @ Maryana2012/Apero-new-project@aa6bed4a19efebffe219d76c20667943f360b6d2 🚀

---
+++ products.2df44495.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".product__list-container"),o=(document.querySelector(".popcorn"),document.querySelector(".buds"),document.querySelector(".all"));console.log(e,o);o.addEventListener("click",(function(){console.log("Button was clicked")}))}();
+//# sourceMappingURL=products.2df44495.js.map
