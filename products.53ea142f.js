b6946af370b68532d2460091eda1e41fbd00af5e
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2024-04-10 11:36:42 +0000

    Deploying to gh-pages from @ Maryana2012/Apero-new-project@639c6573d20565b7ecb03012d7055c7e910a8c1c 🚀

--- products.53ea142f.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".product__list-container"),o=(document.querySelector(".popcorn"),document.querySelector(".buds"),document.querySelector(".all"));console.log(e,o);o.addEventListener("click",(()=>{console.log("Button was clicked")}));
-//# sourceMappingURL=products.53ea142f.js.map
