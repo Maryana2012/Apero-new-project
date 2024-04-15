560b6bcb39f97a7128f71d7b279cc4be48f52692
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2024-04-15 12:28:25 +0000

    Deploying to gh-pages from @ Maryana2012/Apero-new-project@09b7954e95e052269eafbb65db4d940675203157 🚀

---
+++ contacts.386250aa.js
@@ -0,0 +1,2 @@
+const t=window.location.pathname;document.querySelectorAll(".nav__link").forEach((e=>{e.getAttribute("href")===t&&e.classList.add("active__link")}));
+//# sourceMappingURL=contacts.386250aa.js.map
---
+++ contacts.a901390a.js
@@ -0,0 +1,2 @@
+!function(){var t=window.location.pathname;document.querySelectorAll(".nav__link").forEach((function(n){n.getAttribute("href")===t&&n.classList.add("active__link")}))}();
+//# sourceMappingURL=contacts.a901390a.js.map
